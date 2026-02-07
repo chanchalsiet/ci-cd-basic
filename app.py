fac2b11083049b2a0b401ff0b7aae64da4964cf9
Chanchal <137641438+chanchalsiet@users.noreply.github.com>
2026-02-07 12:43:43 -0600

    checking feature branch

--- app.py
+++ app.py
@@ -9,11 +9,20 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
     print("Hello i am learning in deatil ci/cd ");
 
+def add(x,y):
+    return(x+y);
+
+def checking_feature_branch():
+    print("checking_feature_branch")
+
+
 
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print(add(10, 15));
+    checking_feature_branch();
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
