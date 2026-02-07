ee19229f3176ba33d01579ae39587e9ad3e1f1ea
Chanchal <137641438+chanchalsiet@users.noreply.github.com>
2026-02-07 15:40:47 -0600

    add

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,4 @@
-from calculator import add, subtract
+from calculator import add, subtract, multiply
 
 def test_add():
     assert add(2, 3) == 5
@@ -6,4 +6,4 @@ def test_add():
 def test_subtract():
     assert subtract(5, 2) == 3
 def test_multiply():
-    assert test_multiply(2, 3) == 6
+    assert multiply(2, 3) == 6
